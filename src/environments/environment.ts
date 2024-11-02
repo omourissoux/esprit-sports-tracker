3be741e38edc9e38978512673a362072dc9c6f63
omourissoux <73721926+omourissoux@users.noreply.github.com>
2024-11-02 12:10:27 +0100

    Update environment.ts
    ignor env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,19 +2,19 @@ export const environment = {
     production: false,
     stravaBaseUrl: 'https://www.strava.com/api/v3',
     stravaOAuth: {
-      clientId: '101000',
-      redirectUri: 'http://localhost:4200',
+      clientId: '',
+      redirectUri: '',
       issuer: 'https://www.strava.com',
       scope: 'activity:read_all,profile:read_all',
       responseType: 'code',
       loginUrl: 'https://www.strava.com/oauth/authorize',
       oidc: false,
       tokenEndpoint: 'https://www.strava.com/oauth/token',
-      dummyClientSecret: '285d44697ec2d01c351a279fd21c648369c796d9',
+      dummyClientSecret: '',
       showDebugInformation: false,
       logoutUrl: 'https://www.strava.com/oauth/deauthorize',
       revocationEndpoint: 'https://www.strava.com/oauth/deauthorize'
     },
     authBaseUrl: 'https://7ztjdgzh3e.execute-api.ap-southeast-2.amazonaws.com',
-    hostBaseUrl: 'http://localhost:4200'
+    hostBaseUrl: ''
   }
